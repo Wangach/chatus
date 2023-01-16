4debef6422e8b429a5cbb7e1fbc4830b77a3061a
John Kimani <jwangach@gmail.com>
2023-01-16 23:39:09 +0300

    form data capture

--- app.js
+++ app.js
@@ -2,15 +2,15 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5000;
 const router = require('./router/routes');
+const formOptions = {
+    extended: true
+}
 
 
-app.use(router);
+app.use('/users',express.urlencoded(formOptions));
 app.use(express.static('./public'));
+app.use(router);
 
-// app.get('/', (req, res, next) => {
-//     res.send('Hello World!');
-//     next();
-// })
 app.listen(PORT, () => {
     console.log(`Server Is Listening on port ${PORT}`)
 })
--- router/routes.js
+++ router/routes.js
@@ -3,8 +3,6 @@ const express = require('express');
 const app = express();
 const router = express.Router();
 const ejs = require('ejs');
-// const express = require('express');
-// const app = require('express')
 app.set('view engine', 'ejs')
 
 
@@ -12,6 +10,18 @@ app.set('view engine', 'ejs')
 router.get('/', (req, res) => {
     res.status(200).render('../views/login.ejs');
     console.log(`Rendering login page...`)
+})
+router.get('/signup', (req, res) => {
+    res.status(200).render('../views/signup.ejs');
+    console.log(`Rendering Register page...`)
+})
+router.post('/users/signup', (req, res, next) => {
+    res.send(JSON.stringify(req.body));
+    console.log(req.body)
+})
+router.post('/users/login', (req, res, next) => {
+    res.send(JSON.stringify(req.body));
+    console.log(req.body)
 })
 // router.get('/new', (req, res) => {
 //     res.status(200).send('New Guy!')
